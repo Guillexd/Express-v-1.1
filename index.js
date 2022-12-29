2935af444fd0aa85082815f22d3df3158c1c7ae7
GuillexD <enrique2003junior@gmail.com>
2022-12-29 16:47:15 -0500

    nose

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 
 class ProductManager {
-  constructor(){
-    this.path='./db.json';
+  constructor(path){
+    this.path=path;
   }
 
   async addProduct(title, description, price, thumbnail, code, stock){
-
+    
     const product={
-      id: this.#idGenerator(),
+      // id: await this.#idGenerator(),
       title,
       description, 
       price,
@@ -17,45 +17,53 @@ class ProductManager {
       stock
     }
 
-    const verFirst = product[Object.keys(product)[Object.keys(product).length - 1]] !== undefined;
-    const verSecond = await this.#findCode(product.code) ? false : true;
-    console.log(verFirst);
-    console.log(verSecond);
-    try {
-      if (verFirst && verSecond){
-        let db = await fs.promises.readFile(this.path, 'utf-8');
-        db = JSON.parse(db);
+    // const verFirst = await product[Object.keys(product)[Object.keys(product).length - 1]] !== undefined;
+    // const verSecond = await this.#findCode(product.code) ? false : true;
+    let db = await this.#searchDB();
         db.push(product);
         await fs.promises.writeFile(this.path, JSON.stringify(db));
-      } else {
-        console.log(`Required files in product ${product.title} or invalid code`);
-      }
-    } catch (error) {
-      console.log(error);
-    }
+    // try {
+    //   if (verFirst && verSecond){
+    //     let db = await this.#searchDB();
+    //     db.push(product);
+    //     await fs.promises.writeFile(this.path, JSON.stringify(db));
+    //   } else {
+    //     console.log(`Required files in product ${product.title} or invalid code`);
+    //   }
+    // } catch (error) {
+    //   console.log(error);
+    // }
 
   }
 
-  async getProducts(){
-    const getStuffs = await fs.promises.readFile(this.path, 'utf-8')
-    console.log(getStuffs);
-  }
+  // async getProducts(){
+  //   const getStuffs = await fs.promises.readFile(this.path, 'utf-8')
+  //   console.log(getStuffs);
+  // }
 
   // getProductsById(id){
   //   const ver=this.products.find(el=>el.id==id);
   //   console.log(ver || "Not found");
   // }
 
-  #idGenerator(){
-    // const id = this.products.length===0 ? 1 : this.products[this.products.length - 1].id + 1
-    return 1;
+  async #searchDB(){
+    if(!fs.existsSync(this.path)){
+      return [];
+    } else {
+      let db = await fs.promises.readFile(this.path, 'utf-8');
+      db = JSON.parse(db);
+      return db;
+    }
+  }
+
+  async #idGenerator(){
+    const db = await this.#searchDB();
+    const id = db.length===0 ? 1 : db.at(-1).id + 1;
+    return id;
   }
 
   async #findCode(cod){
-    let products = await fs.promises.readFile(this.path, 'utf-8');
-    console.log(typeof(products));
-    products = JSON.parse(products)
-    console.log(typeof(products));
+    const products = await this.#searchDB();
     for (let i in products){
      if (products[i].code==cod){
       return true;
@@ -67,12 +75,12 @@ class ProductManager {
 
 
 
-const newProduct=new ProductManager;
-newProduct.addProduct("Piña","Piña calidad-precio", 1.1,"https://www.lechepuleva.es/documents/13930/203222/pi%C3%B1a_g.jpg/c585227d-e694-464d-87d7-3f2143dd33d9?t=1423480442000",2, 100)
-// newProduct.addProduct("Fresa","Fresa calidad-precio", 2.4, "https://static.wikia.nocookie.net/esharrypotter/images/7/76/Fresa.jpg/revision/latest?cb=20200713112301", 2, 200)
-// newProduct.addProduct("Pan","Pan calidad-precio", 0.5, "https://www.recetasderechupete.com/wp-content/uploads/2018/01/Pan-casero-f%C3%A1cil.jpg", 3, 300)
+const newProduct=new ProductManager('./db.json');
+newProduct.addProduct("Piña","Piña calidad-precio", 1.1,"que", 2, 100);
+newProduct.addProduct("Fresa","Fresa calidad-precio", 2.4, "so", 2, 200);
+newProduct.addProduct("Pan","Pan calidad-precio", 0.5, "xd", 3, 300);
 
-newProduct.getProducts();
+// newProduct.getProducts();
 // newProduct.getProductsById(0);
 
 
@@ -101,3 +109,4 @@ newProduct.getProducts();
 // const product2=[...product, product[1]]
 //     console.log(product);
 //     console.log(product[Object.keys(product)[Object.keys(product).at(-2)]]);
+
