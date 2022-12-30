1152afa4ffa8758d68b20693aa89f403ecf7fb84
GuillexD <enrique2003junior@gmail.com>
2022-12-29 21:34:15 -0500

    Agregando

--- index.js
+++ index.js
@@ -1,50 +1,63 @@
 const fs = require('fs');
 
 class ProductManager {
+
   constructor(path){
     this.path=path;
   }
 
-  async addProduct(title, description, price, thumbnail, code, stock){
-    
-    const product={
-      // id: await this.#idGenerator(),
+  async addProduct(title, des, price, thumbnail, code, stock){
+
+    const product = {
+      id: await this.#idGenerator(),
       title,
-      description, 
+      des,
       price,
       thumbnail,
       code,
       stock
     }
-
-    // const verFirst = await product[Object.keys(product)[Object.keys(product).length - 1]] !== undefined;
-    // const verSecond = await this.#findCode(product.code) ? false : true;
-    let db = await this.#searchDB();
+    const verFirst = product[Object.keys(product)[Object.keys(product).length - 1]] !== undefined;
+    const verSecond = await this.#findCode(product.code) ? false : true;
+    try {
+      if (verFirst && verSecond){
+        let db = await this.#searchDB();
         db.push(product);
-        await fs.promises.writeFile(this.path, JSON.stringify(db));
-    // try {
-    //   if (verFirst && verSecond){
-    //     let db = await this.#searchDB();
-    //     db.push(product);
-    //     await fs.promises.writeFile(this.path, JSON.stringify(db));
-    //   } else {
-    //     console.log(`Required files in product ${product.title} or invalid code`);
-    //   }
-    // } catch (error) {
-    //   console.log(error);
-    // }
-
+        await this.#add(db);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  // async getProducts(){
-  //   const getStuffs = await fs.promises.readFile(this.path, 'utf-8')
-  //   console.log(getStuffs);
-  // }
+  async getProducts(){
+    const db = await this.#searchDB();
+    return db;
+  }
 
-  // getProductsById(id){
-  //   const ver=this.products.find(el=>el.id==id);
-  //   console.log(ver || "Not found");
-  // }
+  async getProductById(id){
+    const db = await this.getProducts();
+    try {
+      const prod = db.find(el => el.id === id);
+      return prod ? prod : "Not found";
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async deleteProductById(id){
+    const db = await this.getProducts();
+    try {
+      const newProducts = db.filter(el => el.id !== id);
+      console.log(newProducts);
+      await this.#add(newProducts);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async deleteAllProducts(){
+    await this.#add([])
+  }
 
   async #searchDB(){
     if(!fs.existsSync(this.path)){
@@ -54,6 +67,10 @@ class ProductManager {
       db = JSON.parse(db);
       return db;
     }
+  }
+
+  async #add(data){
+    await fs.promises.writeFile(this.path, JSON.stringify(data));
   }
 
   async #idGenerator(){
@@ -66,47 +83,15 @@ class ProductManager {
     const products = await this.#searchDB();
     for (let i in products){
      if (products[i].code==cod){
-      return true;
-     }
+        return true;
+      }
     }
   }
-
 }
 
-
-
-const newProduct=new ProductManager('./db.json');
-newProduct.addProduct("Piña","Piña calidad-precio", 1.1,"que", 2, 100);
-newProduct.addProduct("Fresa","Fresa calidad-precio", 2.4, "so", 2, 200);
-newProduct.addProduct("Pan","Pan calidad-precio", 0.5, "xd", 3, 300);
-
-// newProduct.getProducts();
-// newProduct.getProductsById(0);
-
-
-// const leer = fs.readFileSync('./db.json', 'utf-8')
-// const obj={
-// ola: '123xdxdxd'
-// }
-// fs.writeFileSync('./db.json', JSON.stringify(obj));
-
-// const product=[{
-//       title: "title",
-//       description: "des", 
-//       price: "price",
-//       thumbnail: "img",
-//       code: "img",
-//       stock: "stock"
-//     },
-//     {
-//       title: "title1",
-//       description: "des1", 
-//       price: "price1",
-//       thumbnail: "img1",
-//       code: "img1",
-//       stock: "stock1"
-//     }]
-// const product2=[...product, product[1]]
-//     console.log(product);
-//     console.log(product[Object.keys(product)[Object.keys(product).at(-2)]]);
-
+const xd = new ProductManager('./db.json');
+xd.addProduct("Piña","Piña calidad-precio", 1.1,"https://www.lechepuleva.es/documents/13930/203222/pi%C3%B1a_g.jpg/c585227d-e694-464d-87d7-3f2143dd33d9?t=1423480442000", 2, 100);
+// xd.getProducts().then(el=>console.log(el));
+// xd.getProductById(1).then(el=>console.log(el));
+// xd.deleteProductById(0);
+// xd.deleteAllProducts()
