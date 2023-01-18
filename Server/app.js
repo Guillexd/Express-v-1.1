581850544a4cd9ad662fd01382b62efee87e4a82
Guille <enrique2003junior@gmail.com>
2023-01-18 18:44:14 -0500

    Change name's directory

--- Server/app.js
+++ Server/app.js
@@ -1,73 +1,20 @@
 import express from 'express';
-import  { ProductManager } from '../Class/ProductManager.js';
-
-const app = express();
-const productManager = new ProductManager('../DataBase/db.json');
+import RouterProducts from '../Routes/product.router.js';
+import RouterCarts from '../Routes/carts.router.js';
+const app = express()
 
 //default settings
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-//Routers
-const RouterProducts = express.Router();
-app.use('/products', RouterProducts);
-
 //routing
 app.get('/', async(req, res)=>{
-    res.send(`<h1 style='color: salmon; display: grid; place-content: center'>Listening through PORT: ${PORT}</h1>`);
+  res.send(`<h1 style='color: salmon; display: grid; place-content: center'>Listening through PORT: ${PORT}</h1>`);
 })
 
-RouterProducts.get('/', async(req, res)=>{
-    const { limit } = req.query;
-    const evento = await productManager.getProducts(limit || 'max');
-    if(evento){
-        res.status(200).json(evento);
-    } else {
-        res.status(404).json({message: "No Products"});
-    }
-})
-
-RouterProducts.get('/:pId', async(req, res)=>{
-    const { pId } = req.params;
-    const evento = await productManager.getProductById(pId);
-    if(evento){
-        res.status(200).json(evento);
-    } else {
-        res.status(404).json({message: "Not found"});
-    }
-})
-
-RouterProducts.post('/', async(req, res)=>{
-    const obj = req.body;
-    const user = await productManager.addProduct(obj);
-    if(user){
-        res.status(200).json({message: 'Product was added successfully', user});
-    } else {
-        res.status(400).json({message: "There is an error in products"});
-    }
-})
-
-RouterProducts.put('/:idProduct', async(req, res)=>{
-    const { idProduct } = req.params;
-    const obj = req.body;
-    const product = await productManager.updateProductById(parseInt(idProduct), obj);
-    if(product){
-        res.status(200).json({message: 'product was modified successfully', product});
-    } else {
-        res.status(400).json({message: "There is an error in product"});
-    }
-})
-
-RouterProducts.delete('/',async(req,res)=>{
-    productManager.deleteAllProducts();
-    res.json({message:`Products were deleted succesfully`});
-})
-
-RouterProducts.delete('/:idProduct',async(req,res)=>{
-    const { idProduct } = req.params;
-    productManager.deleteProductById(parseInt(idProduct));
-    res.json({message:`Product was deleted succesfully`});
-})
+//Routers
+app.use('/api/products', RouterProducts);
+app.use('/api/carts', RouterCarts);
 
 const PORT = 8080;
 
